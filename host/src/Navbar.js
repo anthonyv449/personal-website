6c843957845a361416184c5e52f1ba7110ddc61c
Anthony Omar Valenzuela <anthonyv449@gmail.com>
2025-01-24 01:32:46 -0800

    add icons, respectable design

--- host/src/Navbar.js
+++ host/src/Navbar.js
@@ -1,69 +1,61 @@
 import React from "react";
-import { Box, Grid2 as Grid, Link, Typography } from "@mui/material";
+import {
+  Box,
+  Grid2 as MuiGrid, // Or Grid2, depending on your MUI version
+  Link,
+  Typography,
+  IconButton,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import { Link as RouterLink } from "react-router-dom";
 
 function NavBar({ pages }) {
   return (
     <Box component="nav">
-      <Grid
+      <MuiGrid
         container
-        direction="row"
-        justifyContent="center" // Centers the entire row horizontally
         alignItems="center"
-        spacing={30}
         sx={{ borderBottom: "2px solid black", height: "2rem" }}
       >
-        <Grid
-          item
-          xs={4}
-          sx={{ display: "flex", alignItems: "center", gap: 2, paddingX: 2 }}
-        >
-          {pages.map((page) => (
-            <Link
-              key={page.id}
-              component={RouterLink}
-              to={page.path}
-              sx={{ textDecoration: "none" }}
-            >
-              {page.title}
-            </Link>
-          ))}
-        </Grid>
-
-        <Grid
-          item
-          xs={4}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
+        {/* Left Column */}
+        <MuiGrid size={6}>
           <Typography variant="h6">valenzuela project</Typography>
-        </Grid>
-        <Grid
-          item
-          xs={4}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            gap: 2,
-            paddingX: 2,
-          }}
-        >
-          {pages.map((link) => (
-            <Link
-              key={link.id}
-              component={RouterLink}
-              to={link.path}
-              sx={{ textDecoration: "none" }}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </Grid>
-      </Grid>
+        </MuiGrid>
+
+        {/* Right (Longer) Column */}
+        <MuiGrid container size={4} alignItems="center" justifyContent="center">
+          {/* Use Box for horizontal spacing */}
+          <MuiGrid container alignItems="center" spacing={3}>
+            {pages.map((page) => (
+              <Link
+                key={page.id}
+                component={RouterLink}
+                to={page.path}
+                sx={{ textDecoration: "none" }}
+              >
+                {page.title}
+              </Link>
+            ))}
+          </MuiGrid>
+        </MuiGrid>
+        <MuiGrid size={2}>
+          <TextField
+            variant="standard"
+            placeholder="Search..."
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+        </MuiGrid>
+      </MuiGrid>
     </Box>
   );
 }
