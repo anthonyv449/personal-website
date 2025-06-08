16d25c19b4fcd939540b87c5219c913707a9a71b
anthonyv449 <anthonyv449@gmail.com>
2025-06-07 20:08:35 -0700

    update production build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "../host/src/index.js", // or wherever your shell entry point is
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/", // for React Router to work
+    clean: true,
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // make sure this file exists
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
