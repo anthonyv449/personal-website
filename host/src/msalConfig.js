b87448d0f609a3ad4805a332ebfea78293bd9bc7
anthonyv449 <anthonyv449@gmail.com>
2025-06-08 16:23:30 -0700

    Update msalConfig.js

--- host/src/msalConfig.js
+++ host/src/msalConfig.js
@@ -1,13 +1,19 @@
 // msalConfig.js
+
 import { PublicClientApplication, LogLevel } from "@azure/msal-browser";
+
+const REDIRECT = window.location.origin;    // e.g. http://localhost:3000 or https://learningwithant.com
 
 export const msalInstance = new PublicClientApplication({
   auth: {
     clientId: "0cf6163b-47fb-4c0c-ab60-9cfc837f00ca",
     authority:
       "https://papichuloant.b2clogin.com/papichuloant.onmicrosoft.com/B2C_1_signin-signout",
-    redirectUri: "http://localhost:3000",
+    redirectUri: REDIRECT,                  // ← dynamic, always correct
+    postLogoutRedirectUri: REDIRECT,        // ← likewise
     knownAuthorities: ["papichuloant.b2clogin.com"],
+    // you can also explicitly set popupRedirectUri to the same if you like:
+    popupRedirectUri: REDIRECT,
   },
   system: {
     loggerOptions: {
