6e91ede7da94642976a14e32e8db77fe245d315d
anthonyv449 <anthonyv449@gmail.com>
2025-06-07 23:26:16 -0700

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
-// webpack.config.js (in root)
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./host/src/index.js", // or your actual entry file
+  entry: "./host/src/index.js",
   output: {
-  path: path.resolve(__dirname, "host/dist"),
-  filename: "bundle.js",
-  publicPath: "/",
-  clean: true,
-},
+    path: path.resolve(__dirname, "host/dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,         // Handle .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],     // Allows importing without extension
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./host/public/index.html", // template source
-      filename: "index.html", // output file
+      template: "./host/public/index.html",
+      filename: "index.html",
     }),
   ],
 };
